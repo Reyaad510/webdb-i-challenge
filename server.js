ce739a0c519ee05959ea59b8b06c354916faff7d
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-22 22:13:27 -0400

    .put functionality completed, added custom middleware

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ server.get('/', async (req, res) => {
   });
 
 
+  // Create - Create account with name and budget
+
   server.post('/', async (req, res) => {
       try {
           if(!req.body.name || !req.body.budget) { 
@@ -34,8 +36,53 @@ server.get('/', async (req, res) => {
   })
 
 
+  
+ // Update - Edit account name and budget 
+
+  server.put('/:id', validateAccount, validateAccountId, async (req,res) => {
+      try {
+        const account = await Accounts.update(req.params.id, req.body);
+        if(account) {
+            res.status(200).json(account)
+        } else {
+            res.status(404).json({ error: "Account not found" })
+        }
+      } catch(err) {
+          console.log(err);
+      }
+  })
 
 
+
+
+// custom middleware 
+
+async function validateAccountId(req, res, next) {
+    try {
+        const { id } = req.params;
+        const account = await Accounts.findById(id);
+    
+        if(account) {
+            req.account = account;
+            next();
+        } else {
+            res.status(400).json({ message: "Invalid account id" })
+    
+        }
+    } catch(err) {
+        res.status(500).json({ message: 'Failed to process request' })
+    }
+    };
+    
+    function validateAccount(req, res, next) {
+     if(req.body && Object.keys(req.body).length) {
+         next();
+     } else if(!req.body) {
+         res.status(400).json({ message: "Missing user data" })
+     } else if(!req.body.name){
+         res.status(400).json({ message: "Missing required name field" })
+     }
+    };
 
   
 
