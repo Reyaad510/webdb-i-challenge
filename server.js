907f12edd6c7d1c854f93afc4fd63332a4f8a357
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-22 22:02:33 -0400

    .post functionality completed

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const Accounts = require('./data/accounts-model');
 
 server.use(express.json());
 
+
+// Read - Obtain accounts
+
 server.get('/', async (req, res) => {
     try {
         const accounts = await Accounts.find();
@@ -14,5 +17,28 @@ server.get('/', async (req, res) => {
         console.log(err);
     }
   });
+
+
+  server.post('/', async (req, res) => {
+      try {
+          if(!req.body.name || !req.body.budget) { 
+           res.status(500).json({ error: "Please fill in a name and budget!" });   
+          } else {
+              const account = await Accounts.add(req.body);
+              res.status(201).json(account);
+          }
+
+      } catch(err) {
+          console.log(err);
+      }
+  })
+
+
+
+
+
+  
+
+
 
 module.exports = server;
