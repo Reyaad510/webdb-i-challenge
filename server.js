819aebbbc5ea7ede70d4676638de4db3482ab693
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-22 21:50:35 -0400

    Created .get for obtaining accounts

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const server = express();
 
+const Accounts = require('./data/accounts-model');
+
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send(`<h2>Testing Testing!</h2>`)
+server.get('/', async (req, res) => {
+    try {
+        const accounts = await Accounts.find();
+        res.status(200).json(accounts);
+    } catch(err) {
+        console.log(err);
+    }
   });
 
 module.exports = server;
